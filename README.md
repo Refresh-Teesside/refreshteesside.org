72dcf0cd3937f3e09550e9148c20dc13f870e321
timgale <tim.gale16@gmail.com>
2013-07-19 15:14:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Refresh Web
+
+The home of the famous Refresh Teesside web application.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Refresh::Application.config.secret_key_base = '54502c2c841bdf8604c66ded8a1ca1775fcf39b8989dabe9e537b786bbf9313a650d26c196d632b6ff8628eac772c97390efa095a3ca8ecc8e02e9e9a2649e67'
