a1becc4de721a4aae07c35debf6f4d1a568320c8
Christopher Scott <chris@roundtablehq.com>
2014-04-28 19:36:07 +0100

    rails admin asset change

--- config/application.rb
+++ config/application.rb
@@ -24,12 +24,12 @@ module Refresh
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
     config.assets.precompile += %w( *.css *.js, *.sass )
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
     # Prevent initializing the application before assets are precompiled (required for heroku)
     config.assets.initialize_on_precompile = false
-    # Add Rails Admin assets (required)
-    config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+    config.assets.enabled = true
 
   end
 end
--- config/initializers/vender.rb
+++ config/initializers/vender.rb
@@ -1,10 +1,10 @@
 require 'eventbrite-client'
 
 eb_auth_tokens = { app_key: ENV['EBAT']}
-eb_client = EventbriteClient.new(eb_auth_tokens)
+@eb_client = EventbriteClient.new(eb_auth_tokens)
 
 
-client = Twitter::REST::Client.new do |config|
+@client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['T_KEY']
   config.consumer_secret     = ENV['T_SECRET']
   config.access_token        = ENV['TAC']
